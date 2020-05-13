abb74c1976c29d2eb7e32a07830b98309bbd6e8b
stszl <stszl@sina.cn>
2020-05-13 22:35:18 +0800

    cocoapods demo

--- STLib/Example/STLib/ViewController.swift
+++ STLib/Example/STLib/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-
+import STLib
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let stView = STView()
+        stView.frame = CGRect.init(x: 0, y: 0, width: 10, height: 10)
+        view.addSubview(stView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
