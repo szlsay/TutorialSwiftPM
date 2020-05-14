fe959b4e2d5c677ab18e260963e484c5dc3f9729
stszl <stszl@sina.cn>
2020-05-14 08:57:42 +0800

    create carthage project

---
+++ STLib/STLib.h
@@ -0,0 +1,19 @@
+//
+//  STLib.h
+//  STLib
+//
+//  Created by 沈兆良 on 2020/5/14.
+//  Copyright © 2020 沈兆良. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STLib.
+FOUNDATION_EXPORT double STLibVersionNumber;
+
+//! Project version string for STLib.
+FOUNDATION_EXPORT const unsigned char STLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STLib/PublicHeader.h>
+
+
